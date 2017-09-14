10a13a7184fed34f5b173e2460f41228a73bc3f7
sandmansandito <sandmansandito@gmail.com>
2017-09-14 17:32:43 -0600

    Updated menu

--- TSK.py
+++ TSK.py
@@ -1,1 +1,46 @@
-import
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+
+#term_width = os.get_terminal_size().columns
+
+
+def print_menu():
+
+    print('''
+
+    TTTTTTTTTTTTTTTTTTTTTTT   SSSSSSSSSSSSSSS KKKKKKKKK    KKKKKKK      =========================================
+    T:::::::::::::::::::::T SS:::::::::::::::SK:::::::K    K:::::K      ||  A project by sandmansandito & derv82
+    T:::::::::::::::::::::TS:::::SSSSSS::::::SK:::::::K    K:::::K      ||
+    T:::::TT:::::::TT:::::TS:::::S     SSSSSSSK:::::::K   K::::::K      ||  Dreamed up under the wet hot neon
+    TTTTTT  T:::::T  TTTTTTS:::::S            KK::::::K  K:::::KKK      ||  lights of the Las Vegas Strip
+            T:::::T        S:::::S              K:::::K K:::::K         ||  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            T:::::T         S::::SSSS           K::::::K:::::K          ||
+            T:::::T          SS::::::SSSSS      K:::::::::::K           ||  A framework for the creation and
+            T:::::T            SSS::::::::SS    K:::::::::::K           ||  deployment of USB/HID based attacks
+            T:::::T               SSSSSS::::S   K::::::K:::::K          ||
+            T:::::T        [v1.0]      S:::::S  K:::::K K:::::K         ||
+            T:::::T                    S:::::SKK::::::K  K:::::KKK      ||
+          TT:::::::TT      SSSSSSS     S:::::SK:::::::K   K::::::K      ||
+          T:::::::::T      S::::::SSSSSS:::::SK:::::::K    K:::::K      ||
+          T:::::::::T      S:::::::::::::::SS K:::::::K    K:::::K      ||
+          TTTTTTTTTTT       SSSSSSSSSSSSSSS   KKKKKKKKK    KKKKKKK      =========================================
+
+          ========================================================
+             THE                SKELETON               KEY                      (H)elp | (A)bout | (Q)uit
+          ========================================================
+
+          * TTTTTTTTTT           SSSSSSSSSS           KKKKKKKKKK *      =========================================
+                                                                        ||
+          M                                                      M      ||
+          E                                                      E      ||
+          N                                                      N      ||
+          U                                                      U      ||
+                                                                        ||
+          * TTTTTTTTTT           SSSSSSSSSS           KKKKKKKKKK *      =========================================
+
+    ''')
+
+
+print_menu()
