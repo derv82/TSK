8758ba45f62775982597d3e56780316286830825
sandmansandito <sandmansandito@gmail.com>
2017-09-23 23:18:54 -0600

    cosmetic changes and dependency checking

--- TSK.py
+++ TSK.py
@@ -9,11 +9,15 @@
 # This is a very experimental first version
 # Hopefully it should improve over time
 
-from colorama import init, Fore, Back, Style
 import os
+import apt
 import sys
+import platform
+from colorama import init, Fore, Back, Style
 
-os.system('clear')
+os.system('reset')
+sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=40, cols=140))
+
 init()
 
 # Set variable shotcuts for colorama colors
@@ -21,42 +25,102 @@ FR = Fore.RED; FW = Fore.WHITE; FG = Fore.GREEN; FB = Fore.BLACK; FY = Fore.YELL
 BLB = Back.LIGHTBLACK_EX;  BR = Back.RED; BW = Back.WHITE; BB = Back.BLUE
 SRST = Style.RESET_ALL; SB = Style.BRIGHT; SD = Style.DIM; SN = Style.NORMAL; BRST = Back.RESET; FRST = Fore.RESET
 
+
+    # Print Main/About Menu
 def print_menu_technicolorama():
+    """
+    Prints the Main/About menu which are one in the same.
+    The colors are defined from colorama and given custom shortcuts to cut down on the amount of bloat.
+    This should also make it easier to change color/style combinations for future updates.
+
+    """
     print('\n\n')
     print('\t' + FR + SB + 'TTTTTTTTTTTTTTTTTTTTTTT   SSSSSSSSSSSSSSS KKKKKKKKK    KKKKKKK' + '      ' + BLB + '                                                     ' + BRST + FRST)
     print('\t' + FR + SB + 'T:::::::::::::::::::::T SS:::::::::::::::SK:::::::K    K:::::K' + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
-    print('\t' + FR + SB + 'T:::::::::::::::::::::TS:::::SSSSSS::::::SK:::::::K    K:::::K' + '      ' + BLB + '  ' + BRST + FG + '   🔑  A framework for the creation and           ' + BLB + '  ' + BRST + FRST)
+    print('\t' + FR + SB + 'T:::::::::::::::::::::TS:::::SSSSSS::::::SK:::::::K    K:::::K' + '      ' + BLB + '  ' + BRST + FG + '     A framework for the creation and           ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + 'T:::::TT:::::::TT:::::TS:::::S     SSSSSSSK:::::::K   K::::::K' + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + 'TTTTTT  T:::::T  TTTTTTS:::::S            KK::::::K  K:::::KKK' + '      ' + BLB + '  ' + BRST + FG + '      deployment of USB/HID based attacks        ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + '        T:::::T        S:::::S              K:::::K K:::::K' + '         ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + '        T:::::T         S::::SSSS           K::::::K:::::K' + '          ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + '        T:::::T          SS::::::SSSSS      K:::::::::::K' + '           ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + '        T:::::T            SSS::::::::SS    K:::::::::::K' + '           ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
-    print('\t' + FR + SB + '        T:::::T               SSSSSS::::S   K::::::K:::::K' + '          ' + BLB + '  ' + BRST + FG + '   💾  A project by sandmansandito & derv82       ' + BLB + '  ' + BRST + FRST)
+    print('\t' + FR + SB + '        T:::::T               SSSSSS::::S   K::::::K:::::K' + '          ' + BLB + '  ' + BRST + FG + '     A project by sandmansandito & derv82       ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + '        T:::::T' + FW +'        [v1.0]' + FR +'      S:::::S  K:::::K K:::::K' + '         ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + '        T:::::T                    S:::::SKK::::::K  K:::::KKK' + '      ' + BLB + '  ' + BRST + FG + '      Dreamed up under the wet hot neon lights   ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + '      TT:::::::TT      SSSSSSS     S:::::SK:::::::K   K::::::K' + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + '      T:::::::::T      S::::::SSSSSS:::::SK:::::::K    K:::::K' + '      ' + BLB + '  ' + BRST + FG + '      of the Las Vegas Strip                     ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + '      T:::::::::T      S:::::::::::::::SS K:::::::K    K:::::K' + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t' + FR + SB + '      TTTTTTTTTTT       SSSSSSSSSSSSSSS   KKKKKKKKK    KKKKKKK' + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
-    print('                                                                            ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
+    print('                                                                            ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST + SRST)
     print(FW + SD + '              ========================================================' + '      ' + SB + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST + SRST)
-    print(FW + SB + '                 THE                  SKELETON                  KEY' + '         ' + SB + BLB + '  ' + BRST + FG + '   📃  Select a menu option on the left to        ' + BLB + '  ' + BRST + FRST + SRST)
+    print(FW + SB + '                 THE                  SKELETON                  KEY' + '         ' + SB + BLB + '  ' + BRST + FG + '     Select a menu option on the left to        ' + BLB + '  ' + BRST + FRST + SRST)
     print(FW + SD + '              ========================================================' + '      ' + SB + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST + SRST)
-    print('                                                                            ' + BLB + '  ' + SB + BRST + FG + '       view payloads for that device.            ' + BLB + '  ' + BRST + FRST+ SRST)
+    print(SB + '                                                                            ' + BLB + '  ' + SB + BRST + FG + '       view payloads for that device.            ' + BLB + '  ' + BRST + FRST+ SRST)
     print('\t' + FW + SB + '      ' + BLB + '*' + '                                                     ' + ' ' + BLB + FW + SB + '*' + BRST + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('                                                                            ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t      ' + FW + BLB + 'T' + BRST + '  [' + FG + SB + 'H' + FW + ']elp  ' + FW + BR + '             Help Me, Rhonda               ' + BRST + ' ' + FW + BLB + 'T' + BRST + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t      ' + FW + BLB + 'S' + BRST + '  [' + FG + SB + 'A' + FW + ']bout ' + FB + BW + '             Check It Out!!!               ' + BRST + ' ' + FW + BLB + 'S' + BRST + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
-    print('\t      ' + FW + BLB + 'K' + BRST + '  [' + FG + SB + 'Q' + FW + ']uit  ' + FW + BB + '             Blow This Joint               ' + BRST + ' ' + FW + BLB + 'K' + BRST + '      ' + BLB + '  ' + BRST + FG + '   📖  Please see the README file or HELP Menu    ' + BLB + '  ' + BRST + FRST)
+    print('\t      ' + FW + BLB + 'K' + BRST + '  [' + FG + SB + 'Q' + FW + ']uit  ' + FW + BB + '             Blow This Joint               ' + BRST + ' ' + FW + BLB + 'K' + BRST + '      ' + BLB + '  ' + BRST + FG + '     Please see the README file or HELP Menu    ' + BLB + '  ' + BRST + FRST)
     print('                                                                            ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t      ' + FW + BLB + 'M' + BRST + '  [' + FG + SB + 'R' + FW + ']ubber Ducky  ' + FY + '     You\'''re The One                 ' + FW + BLB + 'M' + BRST + '      ' + BLB + '  ' + BRST + FG + '      for more information on using TSK          ' + BLB + '  ' + BRST + FRST)
     print('\t      ' + FW + BLB + 'E' + BRST + '  [' + FG + SB + 'B' + FW + ']ash Bunny  ' + FW + '       Payload Puritania              ' + FW + BLB + 'E' + BRST + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST + SRST)
     print('\t      ' + FW + BLB + 'N' + BRST + '  [' + FG + SB + 'T' + FW + ']eensy' + FM + '             Teensy HID Attacks             ' + FW + BLB + 'N' + BRST + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST)
     print('\t      ' + FW + BLB + 'U' + BRST + '  [' + FG + SB + 'U' + FW + ']SB' + FLB + '                USB Flash Drive Dropper        ' + FW + BLB + 'U' + BRST + '      ' + BLB + '  ' + BRST + '                                                 ' + BLB + '  ' + BRST + FRST + SRST)
-    print('                                                                            ' + BLB + '  ' + BRST + SD + '    v1.0 [ The Patriotic Penguin Edition 🐧 ]     ' + BLB + SB + '  ' + BRST + FRST + SRST)
+    print(SB + '                                                                            ' + BLB + '  ' + BRST + SRST + SD + '    v1.0 [ The Patriotic Penguin Edition  ]     ' + BLB + SB + '  ' + BRST + FRST + SRST)
     print('\t' + FW + SB + '      ' + BLB + '*' + '                                                     ' + ' ' + BLB + FW + SB + '*' + BRST + '      ' + BLB + '                                                     ' + BRST + Style.RESET_ALL)
 
+
+def check_deps():
+
+    needed = 0
+
+    apt_cache = apt.Cache()
+
+    linux_distro = platform.linux_distribution()
+
+    # Kali Linux Dependencies
+
+    if 'Kali' in linux_distro:
+        apt_cache.open()
+
+        # if os.path.isdir('/usr/share/fonts-font-awesome/') == True: [ Another way ]
+        if apt_cache["fonts-font-awesome"].is_installed == True:
+
+            print("\t[+] Font-Awesome        [INSTALLED]")
+
+        else:
+
+            print("\t[+] Font-Awesome        [NOT INSTALLED]")
+
+            print("\t\t[-] Installing Font-Awesome [ 'apt-get install fonts-font-awesome' ]")
+            os.system('apt-get install fonts-font-awesome')
+
+            print("\t\t[-] Rebuilding Font Cache [ 'fc-cache -f']")
+            os.system('fc-cache -f')
+
+            needed += 1
+
+        if apt_cache["python3-pip"].is_installed == True:
+
+            print("\t[+] python3-pip         [INSTALLED]")
+
+        else:
+
+            print("\t[+] python3-pip         [INSTALLED]")
+
+            print("\t[-] Installing python3-pip [ 'apt-get install python3-pip' ]")
+            os.system('apt-get install python3-pip')
+
+            print("\t[-] Installing colorama [ 'pip3 install colorama' ]")
+
+        if needed > 0:
+            os.system('python3 ./TSK.py')
+            sys.exit()
+
+        else:
+            input("\n\tPRESS ENTER TO CONTINUE")
+
+            os.system('reset')
 
 menuLoop = True
 
@@ -64,6 +128,7 @@ while menuLoop == True:
 
     try:
 
+        #check_deps()
         print_menu_technicolorama()
 
         choice = input("\n\t      Enter your selection : ")
